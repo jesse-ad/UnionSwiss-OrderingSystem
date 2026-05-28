ebb2de6bb111cede220ae9c4fdfd83012ef047e8
Jesse <admjes003@myuct.ac.za>
2026-05-28 18:28:27 +0200

    Added custom validation method for future delivery date

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,16 @@ class Order < ApplicationRecord
 
   validates :order_number, uniqueness: true, presence: true
   validates :required_delivery_date, presence: true
+
+  # Custom validation for only future delivery dates
+  validate :future_delivery_date
+
+
+  # Checks that date is in the future
+  def future_delivery_date
+    if required_delivery_date.present? && required_delivery_date <= Date.today # If chosen date exists and is before today's date
+      errors.add(:required_delivery_date, "Delivery date must be in the future.") # Error
+    end
+  end
+
 end
