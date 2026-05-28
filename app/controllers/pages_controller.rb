6be80b9ff5dfc1e492d465a8aa88fd7a761a7e82
Jesse <admjes003@myuct.ac.za>
2026-05-28 10:04:41 +0200

    Set up authentication and basic pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def admin
+  end
+
+  def distributor
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root "pages#home"
+  get "admin", to: "pages#admin"
+  get "distributor", to: "pages#distributor"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
