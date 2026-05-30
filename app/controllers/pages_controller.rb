f4c8c4720931777cbefb8a6bc8e16622f4d9690d
Jesse <admjes003@myuct.ac.za>
2026-05-30 10:52:50 +0200

    Added product, distributor, sku, and order count to admin home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,15 @@
 class PagesController < ApplicationController
   def home
+    @product_count = Product.count
+    @distributor_count = Distributor.count
+    @sku_count = Sku.count
+    @order_count = Order.count
   end
 
   def admin
+    
   end
-
   def distributor
+    
   end
 end
