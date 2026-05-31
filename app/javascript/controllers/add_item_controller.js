d4089306a147d1d26272dfb9ffe42d8361c592b0
Jesse <admjes003@myuct.ac.za>
2026-05-31 17:21:11 +0200

    Completed styling for orders and order form

--- app/javascript/controllers/add_item_controller.js
+++ app/javascript/controllers/add_item_controller.js
@@ -1,10 +1,14 @@
- 
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["modal"]
 
+  connect() {
+    alert("CONTROLLER CONNECTED")
+  }
+
   show() {
+    alert("SHOW CLICKED")
     this.modalTarget.style.display = "block"
   }
 
